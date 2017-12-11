560cb5c8a7c06d3e851d8b311e03f2dd0db03b56
ZoeLeBlanc <zgleblanc@gmail.com>
2017-12-11 16:35:23 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var div = document.getElementById('test');
+var myHeading = document.createElement('h1');
+myHeading.textContent = 'Hello world!';
+div.append(myHeading);
+
+let test = true;
+if (test) {
+   alert("Congratulations!! You get a new career.");
+}
